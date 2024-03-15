db73e4546168c214fb0de61a31e7b96bb1d266f7
Mahesh kumar <mahesh.kr.2277@gmail.com>
2024-03-15 19:56:01 +0530

    add: Find The Duplicate Number

---
+++ LinkedList/FindTheDuplicateNumber.cpp
@@ -0,0 +1,25 @@
+
+// time | space
+// O(n) | O(1)
+
+class Solution
+{
+public:
+    int findDuplicate(vector<int> &nums)
+    {
+        int slow = nums[0], fast = nums[0];
+        do
+        {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        fast = nums[0];
+        while (slow != fast)
+        {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
+};
